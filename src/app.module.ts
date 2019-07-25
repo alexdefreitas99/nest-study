2376b676cc64b194144396c9ba3c40fbc6120879
Alex Siqueira de Freitas <alexdefreitas99@hotmail.com>
2019-07-25 11:27:53 -0300

    Modules , imports and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/module/client.module';
+import { ClientService } from './client/providers/client.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
